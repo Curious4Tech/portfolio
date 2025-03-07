1a48329cf5e6199b6fff0f4f77daf5b19af51fd5
Azizou GNANKPE  [ MVP ] <gnankpeazizou@outlook.com>
2025-03-08 00:06:51 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://curios4tech-portfolio-fsgyfkfuf6cbb3cg.canadacentral-01.azurewebsites.net',
-  },
-  poweredByHeader: false,
-  generateEtags: true,
-  // Add specific Azure configurations
-  experimental: {
-    serverActions: true,
-  },
-  // Configure headers for better security
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ]
-  }
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
