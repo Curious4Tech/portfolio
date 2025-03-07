11b0f8ad53d3aa42799ca2e9c0efb7cd942fb73e
Azizou GNANKPE  [ MVP ] <gnankpeazizou@outlook.com>
2025-03-07 21:48:31 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://curios4tech-portfolio-fsgyfkfuf6cbb3cg.canadacentral-01.azurewebsites.net',
+  },
+  // Add these configurations for Azure
+  poweredByHeader: false,
+  generateEtags: true,
 };
 
 export default nextConfig;
